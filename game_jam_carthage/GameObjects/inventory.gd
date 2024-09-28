491b1d4bd8fc3326a57a1889fa00c6ad557dd5a0
MARCHAND MANON <manonmarchand22@gmail.com>
2024-09-28 13:45:20 +0200

    fix: switch to dict to allow eating a certain type of food

--- game_jam_carthage/GameObjects/inventory.gd
+++ game_jam_carthage/GameObjects/inventory.gd
@@ -2,12 +2,12 @@ class_name Inventory
 extends Node
 const enums = preload("res://Singletons/enums.gd")
 
-var inventory: Array[enums.PickableType]
+var inventory: Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	for pickable_type in enums.PickableType:
+		inventory[pickable_type] = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,15 +15,10 @@ func _process(delta: float) -> void:
 	
 # picking is one by one
 func _on_pickable_reveived(pickable: enums.PickableType):
-	inventory.append(pickable)
+	inventory[pickable] = inventory[pickable] + 1
 
-# eating is at night, so monkeys eat a whole Array
-func _on_pickable_eaten(array_eaten: Array[enums.PickableType]):
-	for eaten_counter in array_eaten.size():
-		var eaten_pickable = array_eaten[eaten_counter]
-		for inventory_counter in inventory.size():
-			var item = inventory[inventory_counter]
-			if item == eaten_pickable:
-				inventory.pop_at(inventory_counter)
-				break
+# eating is at night, so monkeys eat a whole bunch
+func _on_pickable_eaten(pickable: enums.PickableType, number_eaten: int):
+	inventory[pickable] = inventory[pickable] - number_eaten
+				
 	
