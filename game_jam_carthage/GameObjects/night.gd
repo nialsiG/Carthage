75ca61898b917c6c34c2e494de7cc8779090a059
MARCHAND MANON <manonmarchand22@gmail.com>
2024-09-28 16:03:42 +0200

    fix: remove print statements and add signal

--- game_jam_carthage/GameObjects/night.gd
+++ game_jam_carthage/GameObjects/night.gd
@@ -1,6 +1,7 @@
 class_name Night
 extends Control
 
+signal night_time
 
 @export var number_of_turns_per_day:int = 20
 
@@ -10,7 +11,6 @@ var is_night: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	is_night = false
 	visible = true
 	$CanvasModulate.color.a = 0
 
@@ -21,22 +21,16 @@ func _process(delta: float) -> void:
 	
 func _on_new_turn(current_turn: int):
 	current_turn = current_turn % number_of_turns_per_day
-	print("current_turn:", current_turn)
 	if current_turn < fully_day:
 		$CanvasModulate.color.a = 0
 	# we did modulo
 	elif current_turn == number_of_turns_per_day - 1:
-		is_night = true
-		print("night")
 		$CanvasModulate.color.a = 1
+		night_time.emit()
 		
 	else:
 		var variable_luminosity: float = number_of_turns_per_day - fully_day
-		print(variable_luminosity)
 		var counter: float = current_turn - fully_day + 1
-		print(counter)
 		var alpha: float = 1 - (variable_luminosity - counter) / variable_luminosity
 		$CanvasModulate.color.a = alpha
-		print(alpha)
-	print("alpha", $CanvasModulate.color.a)
 	
