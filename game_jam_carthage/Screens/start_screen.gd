df876064ce6b09969fc85a861155e480b73e4959
Ghislain Thiery <ghislain.thiery@ntymail.com>
2024-09-28 12:00:32 +0200

    basic menu

--- game_jam_carthage/Screens/start_screen.gd
+++ game_jam_carthage/Screens/start_screen.gd
@@ -1,2 +1,15 @@
 extends Node
 class_name StartScreen
+
+@onready var start_button: Button = %StartButton
+@onready var wiki_button: Button = %WikiButton
+@onready var game_scene =  "res://Screens/GameScreen.tscn"
+
+func _ready():
+	start_button.grab_focus()
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file(game_scene)
+
+func _on_wiki_button_pressed():
+	pass # Replace with function body.
