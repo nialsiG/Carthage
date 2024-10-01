7d9a733d105552f452d485c98871222043568ab6
Lygaenth <115181383+Lygaenth@users.noreply.github.com>
2024-10-01 21:33:09 +0200

    Feature: added reasons for death

--- game_jam_carthage/Screens/night_screen.gd
+++ game_jam_carthage/Screens/night_screen.gd
@@ -16,14 +16,47 @@ func OnDeadMonkeysPublished(deadMonkeys : Array[Monkey], reasons : Array[enums.P
 		monkey_faces.hide()
 		$ColorRect/VBoxContainer/Label.text = "La nuit est tombée...\nTous les singes ont pu manger à leur faim. Tous ont survécu !"
 	else:
-		if deadMonkeys.size() == 1:
-			$ColorRect/VBoxContainer/Label.text = "La nuit est tombée...\nLa nourriture a manqué. Un singe est mort..."
+		var desc = "La nuit est tombée...\n"
+		if reasons.size() == 1:
+			desc+="Il n'y a pas eu assez d"+GetDescSingular(reasons[0])+".\n"
 		else:
-			$ColorRect/VBoxContainer/Label.text = "La nuit est tombée...\nLa nourriture a manqué. "+str(deadMonkeys.size())+" singes sont morts..."
+			var missingFood = "Le groupe a manqué"			
+			for pickable in reasons:
+				missingFood += GetDesc(pickable)+", "
+			missingFood.substr(0, missingFood.length()-1)
+			desc += missingFood+"\n"
+		if deadMonkeys.size() == 1:
+			$ColorRect/VBoxContainer/Label.text = desc + deadMonkeys[0].Name+" est mort..."
+		else:
+			var deadMonkeysDesc : String = ""
+			for i in deadMonkeys.size()-1:
+				deadMonkeysDesc+=deadMonkeys[i].Name+", "
+			deadMonkeysDesc = deadMonkeysDesc.substr(0, deadMonkeysDesc.length()-1)
+			deadMonkeysDesc+=" et "+deadMonkeys[deadMonkeys.size()-1].Name+" sont morts..."
+			$ColorRect/VBoxContainer/Label.text = desc + deadMonkeysDesc
 		monkey_faces.Update(deadMonkeys) 
 		monkey_faces.show()
 
+func GetDesc(pickable : enums.PickableType) -> String:
+	match(pickable):
+		enums.PickableType.LEAF:
+			return "des feuilles"
+		enums.PickableType.GRAIN:
+			return " de l'herbe"
+		enums.PickableType.FRUIT:
+			return "des fruits"
+	return ""
 
+
+func GetDescSingular(pickable : enums.PickableType) -> String:
+	match(pickable):
+		enums.PickableType.LEAF:
+			return "e feuilles"
+		enums.PickableType.GRAIN:
+			return "'herbe"
+		enums.PickableType.FRUIT:
+			return "e fruits"
+	return ""
 func _on_end_night_button_pressed():
 	EndNight.emit()
 	get_tree().paused = false
